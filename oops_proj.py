ba16b8ced42e3162f8ee58ab5729892ebd78062d
Sourav <bhardwajsouav113@gmail.com>
2025-04-02 13:50:14 +0530

    project complete

--- oops_proj.py
+++ oops_proj.py
@@ -12,15 +12,59 @@ class chatbook:
                         4. press 4 to message a friend
                         5. press any other key to exit""")
         if user_input=='1':
-            pass
+            self.signup()
         elif user_input=='2':
-            pass
+            self.signin()
         elif user_input=='3':
-            pass
+            self.my_post()
         elif user_input=='4':
-            pass
+            self.sendmsg()
         else:
             exit()
-        
+    def signup(self):
+        email=input('enter your email here -> ')
+        pwd=input('enter your password here ->') 
+        self.username=email
+        self.password=pwd
+        print('you have signed up successfully')
+        self.menu()
+    def signin(self):
+        if self.username==''and self.password=='':
+            print('please signup first by pressing  1 in the main menu')
+        else:
+            uname=input('enter your email/username here ->')
+            pwd=input('enter your password here ->')
+            if self.username==uname and self.password==pwd:
+                print('you have signed in succesfully')
+                self.loggedin=True
+            else:
+                print('please input correct credentials..')
+        print('\n')
+        self.menu()            
+    def my_post(self):
+        if self.loggedin==True:
+            txt=input('enter your message here -> ')
+            print(f'following content has been posted -> {txt}')
+        else:
+            print('you need to signin first to post something...')
+        print('\n')
+        self.menu()
+    def sendmsg(self):
+        if self.loggedin==True:
+            txt=input('enter your message here ->')
+            frnd=input ('whom to send to {frnd}')
+            print(f'your message has been sent to {frnd}')
+        else:
+            print('you need to signin first to post something...') 
+        print('\n')
+        self.menu()       
+
+
+
+
+
+   
+
+   
 obj=chatbook()        
           
